ddfe404b8f407e9baad9d905709b317eeab8de5e
oneig <oneightwo@mil.ru>
2019-06-15 18:57:36 +0400

    Spring JdbcTemplate

--- src/main/java/com/helloworld/dao/UserDao.java
+++ src/main/java/com/helloworld/dao/UserDao.java
@@ -1,6 +1,10 @@
 package com.helloworld.dao;
 
 import com.helloworld.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -13,70 +17,31 @@ import java.util.Properties;
 @Component
 public class UserDao {
 
-    private static Connection connection;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-    static {
-        String url = null;
-        String username = null;
-        String password = null;
-
-        //load db properties
-        try(InputStream in = UserDao.class
-                .getClassLoader().getResourceAsStream("persistence.properties")) {
-            Properties properties = new Properties();
-            properties.load(in);
-            url = properties.getProperty("url");
-            username = properties.getProperty("username");
-            password = properties.getProperty("password");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //acquire db connection
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public List<User> getAll() throws SQLException {
-        List<User> users = new ArrayList<>();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            User user = new User();
-            user.setName(resultSet.getString(1));
-            user.setSurname(resultSet.getString(2));
-            user.setEmail(resultSet.getString(3));
-            users.add(user);
-        }
-        return users;
+    public List<User> getAll() {
+        return jdbcTemplate.query("SELECT * FROM users",
+                new BeanPropertyRowMapper<>(User.class));
+//        return jdbcTemplate.query("SELECT * FROM users", (resultSet, i) -> {
+//            User user = new User();
+//            user.setName(resultSet.getString(1));
+//            user.setSurname(resultSet.getString(2));
+//            user.setEmail(resultSet.getString(3));
+//            return user;
+//        });
     }
 
     public User getOne(String email) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE email=?");
-            preparedStatement.setString(1, email);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                User user = new User();
-                user.setName(resultSet.getString(1));
-                user.setSurname(resultSet.getString(2));
-                user.setEmail(resultSet.getString(3));
-                return user;
-            }
-        } catch (SQLException ignored) {
-        }
-        return null;
+        return jdbcTemplate.query(
+                "SELECT * FROM users WHERE email = ?",
+                new Object[] {email},
+                new BeanPropertyRowMapper<>(User.class)
+        ).stream().findAny().orElse(null);
     }
 
-    public void add(User user) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users VALUES (?, ?, ?)");
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getSurname());
-        preparedStatement.setString(3, user.getEmail());
-        preparedStatement.execute();
+    public void add(User user) {
+        jdbcTemplate.update("INSERT INTO users VALUES (?, ?, ?)",
+                user.getName(), user.getSurname(), user.getEmail());
     }
 }
