cf14e27c1385275e28ff496a1e1d759a9a732903
oneig <oneightwo@mil.ru>
2019-06-19 13:07:09 +0400

    Spring AOP

---
+++ src/main/java/com/helloworld/aspect/LogAspect.java
@@ -0,0 +1,21 @@
+package com.helloworld.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogAspect {
+
+    public void beforeServiceMethodInvocation(JoinPoint joinPoint) {
+        System.out.println("Invocation of method " + joinPoint.getSignature());
+    }
+
+    public Object aroundServiceMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object res = joinPoint.proceed();
+        long end = System.currentTimeMillis();
+        System.out.println("Execution of method " + joinPoint.getSignature() + " took " + (end - start) + "msec.");
+        return res;
+    }
+}
